395a0a4fae7d242e6d9d9a6437bfa40a5ae95e81
Mohammed Ameen <127188975+AmeenMohammed03@users.noreply.github.com>
2024-01-29 16:06:41 +0530

    new tests

---
+++ app/src/test/java/com/example/currencyconverter/SampleUnitTest.kt
@@ -0,0 +1,12 @@
+package com.example.currencyconverter
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SampleUnitTest {
+
+    @Test
+    fun division() {
+        assertEquals(1224, 25 / 2 * 2 + 200 * 6)
+    }
+}
